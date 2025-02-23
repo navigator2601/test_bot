bd56345505d6b0313a74477f405d59be1a7074d8
navigator2601 <navigator.vip14@gmsil.com>
2025-02-23 16:45:25 +0200

    Оновлення додатку ver:0.0001

--- run_test_bot.py
+++ run_test_bot.py
@@ -1,8 +1,13 @@
 import os
-from bot import Bot
+import sys
+
+# Додавання шляху до поточної директорії для пошуку модулів
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 # Завантаження конфігурації для тестового середовища
 os.environ['ENV_FILE'] = '.env.test'
+
+from bot import Bot
 
 # Ініціалізація бота з тестовими налаштуваннями
 bot = Bot()
