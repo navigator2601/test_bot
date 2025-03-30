f58d284e011b11799b811e171f84532be65dfe2b
navigator2601 <navigator.vip14@gmsil.com>
2025-03-30 20:39:44 +0300

    Remove all files

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()  # Завантажує змінні середовища з .env
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")  # Телеграм токен
-DATABASE_URL = os.getenv("DATABASE_URL")  # Підключення до Postgres
