8758b53a6170cf871ba31c60a0231bfd6d750326
navigator2601 <navigator.vip14@gmsil.com>
2025-04-12 21:44:51 +0300

    Бот телеграм v.0.0.0.0

--- handlers/__init__.py
+++
@@ -1,7 +0,0 @@
-from aiogram import Dispatcher
-from .start import router as start_router
-from .user import router as user_router  # Імпортуємо обробники для команд користувача
-
-def register_handlers(dp: Dispatcher):
-    dp.include_router(start_router)  # Реєструємо обробники для /start
-    dp.include_router(user_router)  # Реєструємо обробники для команд користувача
