8d67aebff02879c54c6ac1ac1f41a465d5edecf8
navigator2601 <navigator.vip14@gmsil.com>
2025-04-12 16:51:35 +0300

    Бот телеграм 0.0.2

--- keyboards.py
+++ keyboards.py
@@ -1,4 +1,4 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
 
 def main_menu() -> ReplyKeyboardMarkup:
     """Головне меню 2×2 з кнопками та піктограмами"""
@@ -14,7 +14,13 @@ def catalog_menu() -> ReplyKeyboardMarkup:
     """Компактне підменю для кнопки Каталог (2×2)"""
     return ReplyKeyboardMarkup(
         keyboard=[
-            [KeyboardButton(text="📜 Вибір зі списку"), KeyboardButton(text="🔍 Пошук по бренду")],
+            [
+                KeyboardButton(
+                    text="📜 Вибір зі списку",
+                    web_app=WebAppInfo(url="https://navigator2601.github.io/test_bot/webapp/")  # URL вашого веб-додатка
+                ),
+                KeyboardButton(text="🔍 Пошук по бренду")
+            ],
             [KeyboardButton(text="📂 Пошук по типу"), KeyboardButton(text="⬅️ Назад")]  # Оновлений формат
         ],
         resize_keyboard=True
